87805358889cd6a1237a23f6b1262e0ae6ab3d85
Holandara <saraholandatavares@gmail.com>
2024-10-17 16:03:15 -0300

    Glassmorphism

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import background from './elementos/backgroundimg1.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+      <div class="container">
+
+        <div class="row">
+          
+            <div className="col-4">
+              <div className="blured">teste</div>
+          </div>
+
+          <div className="col-4">
+            <img className="imagem1" src={background} width="100%" height="100%"/>
+          
+        </div>
+
+        </div>
+      </div>
       </header>
     </div>
   );

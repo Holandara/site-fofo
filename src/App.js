0871155eb0bddf983d7d37cbefa500bdb58ada5d
Holandara <saraholandatavares@gmail.com>
2024-10-18 09:11:47 -0300

    Font and text editing

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ function App() {
     <div className="App">
       <header >
       <div class="container">
-
         <div class="row">
-          
-            <div className="col-4">
-              <div className="blured">teste</div>
-          </div>
 
+          {/* Efeito blur por cima das plantas */}
+          <div className="col-4">
+              <div className="blured">
+                <div className="conteudo-titulo">
+                  Landing page
+                </div>
+              </div>
+          </div>
+          
+          {/* Imagem das plantas */}
           <div className="col-4">
             <img className="imagem1" src={background} width="100%" height="100%"/>
-          
-        </div>
+          </div>
 
         </div>
       </div>
@@ -26,3 +30,5 @@ function App() {
 }
 
 export default App;
+
+
